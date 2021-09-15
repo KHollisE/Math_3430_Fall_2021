df4293bd8aed4e4bb23040fd20237064b6ee6f57
KHollisE <90790329+KHollisE@users.noreply.github.com>
2021-09-15 12:34:31 -0500

    This is a test to see if I can commit files

---
+++ Vectors_lists.py
@@ -0,0 +1,22 @@
+vector_01 = [1,2,3]
+
+vector_02 = [3,2,1]
+
+vector_a = [1,0,0]
+vector_b = [0,1,0]
+vector_c = [0,0,1]
+
+identity_3x3 = [vector_a,vector_b, vector_02]
+
+matrix_3x3 = [vector_01,vector_a,vector_02]
+
+print(matrix_3x3)
+
+
+""""""
+"matrix_3x3 is"
+
+"0 1 0"
+"1 0 0"
+"0 0 1"
+""""""
